bf1a748e6b88ef68035af0b6f5480b6c36a1446a
Alibek Manabayev <m-nny@yandex.ru>
2020-04-22 06:42:28 +0600

    player collision

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-
-export const SCALE = 4;
-
-export const TILE_WIDTH = 16 * SCALE;
-export const TILE_HEIGHT = 16 * SCALE;
-
-export const ROWS = 8;
-export const COLUMNS = 8;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as Phaser from 'phaser';
 import tiles from '../assets/0x72_DungeonTilesetII_v1.3.png';
+import reqSquare from '../assets/red_square.png';
 import tilemap from '../assets/map.json';
 import lizard from '../assets/sprites/lizard_f_idle_anim_f4.png';
 
@@ -9,49 +10,58 @@ let sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   key: 'Game',
 }
 
+type ArdaceSprite = Phaser.GameObjects.Sprite & { body: Phaser.Physics.Arcade.Body };
 class GameScene extends Phaser.Scene {
-  private player!: Phaser.GameObjects.Sprite & { body: Phaser.Physics.Arcade.Body };
+  private player!: ArdaceSprite;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-  private debugGraphics!: Phaser.GameObjects.Graphics;
   private map!: Phaser.Tilemaps.Tilemap;
-  private showDebug = false;
 
   constructor() {
     super(sceneConfig);
   }
   public preload() {
     this.load.image('dungeon', tiles)
+    this.load.image('red_square', reqSquare)
     this.load.tilemapTiledJSON('map', tilemap);
     this.load.spritesheet('player', lizard, { frameWidth: 16, frameHeight: 28, });
   }
   public create() {
     this.map = this.make.tilemap({ key: 'map' });
-    let tiles = this.map.addTilesetImage('0x72_DungeonTilesetII_v1.3', 'dungeon');
-    let floor = this.map.createStaticLayer('Floor', tiles);
-    let walls = this.map.createStaticLayer('Walls', tiles);
-    walls.setCollisionByProperty({ collides: true });
-
+    let dungeionTileset = this.map.addTilesetImage('0x72_DungeonTilesetII_v1.3', 'dungeon');
+    let floor = this.map.createStaticLayer('Floor', dungeionTileset);
+    let obstacles = this.createObstacles();
+    
+    let walls = this.map.createStaticLayer('Walls', dungeionTileset);
+    let wallTopings = this.map.createStaticLayer('WallTopings', dungeionTileset);
     this.player = this.map.createFromObjects('Player', 'StartingPoint', { key: 'player' })[0] as any;
-    this.player.setSize(16, 16);
+    // this.player.setSize(16, 8);
     this.physics.add.existing(this.player);
-    this.physics.add.collider(walls, this.player);
-    this.player.body.setOffset(0, 12);
+    this.player.body.setOffset(0, 16);
+
+    this.physics.add.collider(obstacles, this.player);
 
     this.cameras.main.startFollow(this.player);
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
 
     this.cameras.main.setZoom(2);
 
-    this.debugGraphics = this.add.graphics();
-
     this.input.keyboard.on('keydown_C', () => {
-      this.showDebug = !this.showDebug;
-      this.drawDebug();
+      this.physics.world.debugGraphic.clear();
+      this.physics.world.drawDebug = !this.physics.world.drawDebug;
     })
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
-  public update(_: never, delta: number) {
+  private createObstacles(): ArdaceSprite[] {
+    const obstacles = this.map.createFromObjects('Obstacles', 'Wall', { key: 'red_square' }) as ArdaceSprite[];
+    obstacles.forEach((obs) => {
+      this.physics.add.existing(obs);
+      obs.setVisible(false).setActive(true);
+      obs.body.setImmovable(true).setSize(obs.width, obs.height);
+    });
+    return obstacles;
+  }
+  public update() {
     this.player.body.setVelocity(0);
 
     if (this.cursors.up?.isDown) {
@@ -64,16 +74,6 @@ class GameScene extends Phaser.Scene {
       this.player.body.setVelocityX(100);
     } else if (this.cursors.left?.isDown) {
       this.player.body.setVelocityX(-100)
-    }
-  }
-  private drawDebug() {
-    this.debugGraphics.clear();
-    if (this.showDebug) {
-      this.map.renderDebug(this.debugGraphics, {
-        tileColor: null,
-        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 200),
-        faceColor: new Phaser.Display.Color(40, 39, 37, 255),
-      })
     }
   }
 }
